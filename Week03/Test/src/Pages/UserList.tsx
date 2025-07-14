ae35791f3da732367570429e8c78d0b642436e40
Alienn704 <nguyentngoclien0704@gmail.com>
2025-07-14 10:29:15 +0700

    update

--- Week03/Test/src/Pages/UserList.tsx
+++ Week03/Test/src/Pages/UserList.tsx
@@ -1,6 +1,6 @@
-
+import React from "react";
 import { useUser } from "./UserProvider";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 
 const UserList = () => {
   const { users } = useUser();
@@ -8,15 +8,37 @@ const UserList = () => {
   return (
     <div>
       <h3>User List</h3>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <Link to={`/users/${user.id}`}>
-              {user.name} - {user.email} - {user.age ?? "N/A"}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <table border={1} cellPadding={8} cellSpacing={0}>
+        <thead>
+          <tr style={{ backgroundColor: "#f0f0f0" }}>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Age</th>
+            <th>Detail</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.age ?? "N/A"}</td>
+              <td>
+                <Link to={`/users/${user.id}`}>View</Link>
+              </td>
+            </tr>
+          ))}
+          {users.length === 0 && (
+            <tr>
+              <td colSpan={5} style={{ textAlign: "center" }}>
+                No users found
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
